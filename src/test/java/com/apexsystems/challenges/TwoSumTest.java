d081731483debd8165651a3c0a18a1112bbb4867
saperez <saperez@apexsystems.com>
2024-08-30 11:35:27 -0600

    Added code challenge TwoSum

---
+++ src/test/java/com/apexsystems/challenges/TwoSumTest.java
@@ -0,0 +1,22 @@
+package com.apexsystems.challenges;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class TwoSumTest {
+
+    @Test
+    public void twoSumTest() {
+        int[] nums1 = new int[]{-1,-2,-3,-4,-5};
+        int target1 = -8;
+        int[] answer1 = new int[]{2,4};
+        Assert.assertArrayEquals(answer1, TwoSum.twoSum(nums1, target1));
+
+        int[] nums2 = new int[]{9,7,13,25,6,19,14,4,32,20};
+        int target2 = 24;
+        int[] answer2 = new int[]{7,9};
+        Assert.assertArrayEquals(answer2, TwoSum.twoSum(nums2, target2));
+    }
+}
